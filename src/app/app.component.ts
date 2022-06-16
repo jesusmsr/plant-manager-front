893e18fc19cb5def91f5d68ca82b5917a3d6e052
jesusmsr <jsr20xx@gmail.com>
2022-06-16 16:29:48 +0200

    Handle bad credentials on log in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStorageService } from './services/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'plantmanager';
+  isLoggedIn: boolean = false;
+
+  constructor(
+    private token: TokenStorageService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.token.getToken();
+    if (this.isLoggedIn) {
+      this.router.navigate(['home']);
+    }
+  }
+
+  logout(): void {
+    this.token.signOut();
+    window.location.reload();
+  }
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-login-page',
@@ -8,10 +9,13 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  errorMsg?: string;
 
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
+    private token: TokenStorageService
   ) { }
 
   loginForm = this.formBuilder.group({
@@ -25,10 +29,31 @@ export class LoginPageComponent implements OnInit {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      this.authService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe(response => {
-
-      });
+      this.authService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe((response: any) => {
+        console.log(response);
+        this.token.saveToken(response.token.access);
+        this.token.saveUser(response);
+        this.isLoggedIn = true;
+        this.reloadPage();
+      },
+        (error) => {
+          this.errorMsg = error.error.error;
+          console.log(this.getApiErrors(this.errorMsg));
+        });
     }
+  }
+
+  getApiErrors(errorMsg: any): string {
+    switch (errorMsg) {
+      case 'badCredentials':
+        return 'Bad credentials';
+      default:
+        return 'Unknown error';
+    }
+  }
+
+  reloadPage(): void {
+    window.location.reload();
   }
 
 }
